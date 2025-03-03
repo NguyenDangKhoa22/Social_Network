5c0a7ebcce3cc2d654f99c6f0438620c1375b045
NguyenDangKhoa22 <khoa.nd.github@gmail.com>
2025-03-03 16:01:44 +0700

    Model interaction in spring boot

---
+++ backend/src/main/java/com/example/backend/service/UserService.java
@@ -0,0 +1,24 @@
+package com.example.backend.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.backend.dto.UserCreationRequest;
+import com.example.backend.entity.User;
+import com.example.backend.repository.UserRepository;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public User creatRequest(UserCreationRequest request){
+        User user = new User();
+        user.setUsername(request.getUsername());
+        user.setPassword(request.getPassword());
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+        user.setDob(request.getDob());
+        return userRepository.save(user);
+    }
+}
